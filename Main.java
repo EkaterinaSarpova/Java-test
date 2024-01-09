a1692e5ba5a63de8cc978667e6a70da16599b63c
EkaterinaSarpova <155678463+EkaterinaSarpova@users.noreply.github.com>
2024-01-09 15:29:00 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
-class Calc {
+class Main {
 
     public static void main (String [] args) throws Exception {
         Scanner scanner = new Scanner (System.in);
